424a45f6af8107848fd9d86945afbca6a0ead7ac
AntinomyCC <163209079+AntinomyCC@users.noreply.github.com>
2026-08-18 18:25:49 +0200

    Replaced the double circle with vector calculations in convert_photo_to_hsl to speed up the program

--- PhotoDealer.py
+++ PhotoDealer.py
@@ -25,45 +25,36 @@ class Photo:
 
     def convert_photo_to_hsl(self):
         if self.photo_rgb is None:
-            self.photo_read()
-            
+            self.photo_read()          
         photo_rgb = self.photo_rgb
-        red_channel = photo_rgb[:,:,0].astype(float)/255
-        green_channel =photo_rgb[:,:,1].astype(float)/255
-        blue_channel = photo_rgb[:,:,2].astype(float)/255 
 
-        (pixels_vertical, pixels_horizontal)= red_channel.shape
-        photo_hsl = np.zeros((pixels_vertical,pixels_horizontal,3)) # allocate the room for the converted photo. 
+        photo_rgb_normalized = photo_rgb/255.0
+        photo_hsl = np.zeros(photo_rgb.shape)
 
-        for i in range(0,pixels_vertical):
-            for j in range(0,pixels_horizontal):
-                min = np.min([red_channel[i,j],green_channel[i,j],blue_channel[i,j]])
-                max = np.max([red_channel[i,j],green_channel[i,j],blue_channel[i,j]])
-                idxmax = np.argmax([red_channel[i,j],green_channel[i,j],blue_channel[i,j]])
-
-                delta = max - min
-                L = (min + max) /2
-                photo_hsl[i,j,2] = L
-                if delta == 0:
-                    H = 0
-                elif idxmax == 0:
-                    H = 60 * (((green_channel[i,j] - blue_channel[i,j])/delta) % 6)
-                elif idxmax == 1:
-                    H = 60 * ((blue_channel[i,j] - red_channel[i,j]) / delta + 2)
-                elif idxmax == 2:
-                    H = 60 * ((red_channel[i,j] - green_channel[i,j]) / delta + 4)
-
-                photo_hsl[i,j,0] = H 
-
-                if delta == 0:
-                    S = 0
-                elif L <= 0.5:
-                    S = delta/(max + min)
-                else:
-                    S = delta/(2 - max - min)
-
-                photo_hsl[i,j,1] = S
-                self.photo_hsl = photo_hsl
+        min_rgb_normalized = np.min(photo_rgb_normalized, axis=2)
+        max_rgb_normalized = np.max(photo_rgb_normalized, axis=2)
+        idxmax_rgb_normalized = np.argmax(photo_rgb_normalized, axis=2)
+        delta_rgb_normalized = max_rgb_normalized - min_rgb_normalized
+        photo_hsl[:,:,2] = (min_rgb_normalized+max_rgb_normalized)/2
+        
+        photo_hsl[:,:,0] = np.select([delta_rgb_normalized == 0, 
+                   idxmax_rgb_normalized == 0, 
+                   idxmax_rgb_normalized == 1, 
+                   idxmax_rgb_normalized == 2
+                   ],
+                   [0.0,
+                    60*((photo_rgb_normalized[:,:,1]-photo_rgb_normalized[:,:,2])/delta_rgb_normalized)%6,
+                    60*((photo_rgb_normalized[:,:,2]-photo_rgb_normalized[:,:,0])/delta_rgb_normalized)+2,
+                    60*((photo_rgb_normalized[:,:,0]-photo_rgb_normalized[:,:,1])/delta_rgb_normalized)+4
+                    ])
+        photo_hsl[:,:,1] = np.select([delta_rgb_normalized==0,
+                                      photo_hsl[:,:,2] <= 0.5,
+                                      photo_hsl[:,:,2] >0.5
+                                      ],
+                                      [0,
+                                       delta_rgb_normalized/(max_rgb_normalized + min_rgb_normalized),
+                                       delta_rgb_normalized/(2.0 - max_rgb_normalized - min_rgb_normalized)])
+        self.photo_hsl = photo_hsl
             
     def saturation_tuner(self):
         pass
@@ -78,7 +69,7 @@ def main():
     photo = Photo(path)
     photo.photo_read()
     photo.convert_photo_to_hsl()
-    plt.imshow(photo.photo_hsl)
+    plt.imshow(photo.photo_rgb  )
     plt.show()
 
 if __name__ == "__main__":
